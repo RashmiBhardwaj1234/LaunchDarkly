fddbf21ee565011546827f9a295648eccdc5d1bc
Jay Winstead <jwinstead@launchdarkly.com>
2018-04-17 12:45:28 -0700

    show example using a HashMap as custom attributes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,8 @@
 import com.launchdarkly.client.LDClient;
 import com.launchdarkly.client.LDUser;
+import com.launchdarkly.client.LDUser.Builder;
+
+import java.util.*;
 
 import java.io.IOException;
 
@@ -10,11 +13,25 @@ public class Hello {
  public static void main(String... args) throws IOException {
    LDClient client = new LDClient("YOUR_SDK_KEY");
 
-   LDUser user = new LDUser.Builder("bob@example.com")
+   Builder userBuilder = new LDUser.Builder("bob@example.com")
                            .firstName("Bob")
                            .lastName("Loblaw")
-                           .customString("groups", singletonList("beta_testers"))
-                           .build();
+                           .customString("groups", singletonList("beta_testers"));
+
+   HashMap<String, List<String>> mapOfCustomAttributes = new HashMap<String, List<String>>();
+   mapOfCustomAttributes.put("groups", Arrays.asList("mbs_testers", "testing"));
+   mapOfCustomAttributes.put("mbs_traders", Arrays.asList("trader1", "trader2","trader3"));
+   mapOfCustomAttributes.put("mbs_special_users", Arrays.asList("mbs_spec_1"));
+
+   for (Map.Entry<String, List<String>> entry : mapOfCustomAttributes.entrySet()) {
+     String key = entry.getKey();
+     List<String> list = entry.getValue();
+     System.out.println("Custom attribute Key: " + key);
+     System.out.println("Custom attribute List: " + list);
+     userBuilder.customString(key, list);
+   }
+
+   LDUser user = userBuilder.build();
 
    boolean showFeature = client.boolVariation("YOUR_FEATURE_KEY", user, false);
 
